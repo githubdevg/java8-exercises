b6d27a2a2cc385ec4dce858e9fb1c38cf7160c89
githubdevg <zeta_x_07@hotmail.com>
2019-02-10 18:30:54 -0600

    Comparator Comparing
    - Review of Comparator.comparing

--- src/test/java/mx/devg/comparator/ComparatorTest.java
+++ src/test/java/mx/devg/comparator/ComparatorTest.java
@@ -2,12 +2,17 @@ package mx.devg.comparator;
 
 import mx.devg.comparator.model.Employee;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.Comparator;
+
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class ComparatorTest {
 
+    private Employee[] employees = null;
     private Employee[] sortedEmployeesByName = null;
     private Employee[] sortedEmployeesByNameDesc = null;
     private Employee[] sortedEmployeesByAge = null;
@@ -15,30 +20,51 @@ public class ComparatorTest {
 
     @Before
     public void init() {
-        sortedEmployeesByName = new Employee[]{new Employee("Ace", 22, 2000d, 5924001l),
-                new Employee("John", 25, 3000d, 9922001l),
-                new Employee("Keith", 35, 4000d, 3924401l)};
+        Employee employee1 = new Employee("Aceeee", 22, 2000d, 5924001l);
+        Employee employee2 = new Employee("John", 25, 3000d, 9922001l);
+        Employee employee3 = new Employee("Keith", 35, 4000d, 3924401l);
 
-        sortedEmployeesByNameDesc = new Employee[]{new Employee("Keith", 35, 4000d, 3924401l),
-                new Employee("John", 25, 3000d, 9922001l),
-                new Employee("Ace", 22, 2000d, 5924001l)};
+        employees = new Employee[] {employee2, employee1, employee3};
 
-        sortedEmployeesByAge = new Employee[]{new Employee("Ace", 22, 2000d, 5924001l),
-                new Employee("John", 25, 3000d, 9922001l),
-                new Employee("Keith", 35, 4000d, 3924401l)
-        };
+        sortedEmployeesByName = new Employee[]{employee1, employee2, employee3};
 
+        sortedEmployeesByNameDesc = new Employee[]{employee3, employee2, employee1};
+
+        sortedEmployeesByAge = new Employee[]{employee1, employee2, employee3};
+
+    }
+
+    @Test
+    public void whenComparing_thenSortedByName() {
+        Comparator<Employee> employeeNameComparator = Comparator.comparing(e -> e.getName());
+
+        printArray(employees);
+
+
+        Arrays.sort(employees, new EmployeeByNameComparator());
+
+        printArray(employees);
+
+        assertArrayEquals(employees, sortedEmployeesByName);
 
     }
 
 
-//    public static void main(String[] args) {
-//
-//        Stream<Employee> employeeStream = Arrays.stream(employees);
-//
-//        employeeStream.forEach(e -> System.out.println(e.getName()));
-//
-//
-//    }
+    public void printArray(Employee[] employeeArray) {
+        Arrays.stream(employeeArray).forEach(e -> System.out.println("Name: " + e.getName()));
+    }
+
+    class EmployeeByNameComparator implements Comparator<Employee>{
+
+        @Override
+        public int compare(Employee e1, Employee e2) {
+            return e1.getName().compareToIgnoreCase(e2.getName());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return false;
+        }
+    }
 
 }
